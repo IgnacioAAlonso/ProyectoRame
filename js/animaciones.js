88f4234bca98dcb6b07586ad0b83382faf6ef818
ignacio alonso <ignacioanacional@gmail.com>
2021-10-25 16:59:05 -0300

    Commit de modificaciones en las animaciones

--- js/animaciones.js
+++ js/animaciones.js
@@ -24,13 +24,66 @@ jQuery(() => {
                 informacion.animate({opacity: 1});
             }
 
-            for (let i = 0; i < galeria.length; i++) {
-               let alturaGaleria = galeria.offset().top;
-                if((alturaGaleria - 750) < scroll){
-                    galeria.addClass("mostrarDerecha");
-                    galeria.animate({opacity: 1})
-                }
+            
+            let alturaGaleria = galeria.offset().top;
+            if((alturaGaleria - 750) < scroll){
+                $(`#card1`).addClass("mostrarDerecha");
+                $(`#card1`).animate({opacity: 1},
+                    function(){
+                        $(`#card2`).addClass("mostrarDerecha");
+                        $(`#card2`).animate({opacity: 1},
+                            function(){
+                                $(`#card3`).addClass("mostrarDerecha");
+                                $(`#card3`).animate({opacity: 1},
+                                    function(){
+                                        $(`#card4`).addClass("mostrarDerecha");
+                                        $(`#card4`).animate({opacity: 1},
+                                            function(){
+                                                $(`#card5`).addClass("mostrarDerecha");
+                                                $(`#card5`).animate({opacity: 1},
+                                                    function(){
+                                                        $(`#card6`).addClass("mostrarDerecha");
+                                                        $(`#card6`).animate({opacity: 1},
+                                                            function(){
+                                                                $(`#card7`).addClass("mostrarDerecha");
+                                                                $(`#card7`).animate({opacity: 1},
+                                                                    function(){
+                                                                        $(`#card8`).addClass("mostrarDerecha");
+                                                                        $(`#card8`).animate({opacity: 1});
+                                                                    });
+                                                            });
+                                                    });
+                                            });
+                                    });
+                            });
+                    });
             }
+            
          });  
       });
+
+    $("#nav_jabones, #nav_jabones2").on("click", function(){
+        $("html, body").animate({
+            scrollTop: $("#section_jabones").offset().top}, 1000);
+    });
+
+    $("#nav_velas, #nav_velas2").on("click", function(){
+        $("html, body").animate({
+            scrollTop: $("#3").offset().top}, 1000);
+    });
+
+    $("#nav_nosotros, #nav_nosotros2").on("click", function(){
+        $("html, body").animate({
+            scrollTop: $("#section_nosotros").offset().top}, 1000);
+    });
+
+    $("#nav_contacto, #nav_contacto2").on("click", function(){
+        $("html, body").animate({
+            scrollTop: $("#section_contacto").offset().top}, 1000);
+    });
+
+    $("#nav_inicio").on("click", function(){
+        $("html, body").animate({
+            scrollTop: $("#section_inicio").offset().top}, 1000);
+    });
 });
